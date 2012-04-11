fd01795ed6da67ae83b2cad4c739bab6686b2537
Matthew Harris <polyharris@yahoo.com>
2012-04-10 17:41:06 -0700

    Updating scrolling errors

--- gen/org/getnote/R.java
+++ gen/org/getnote/R.java
@@ -25,28 +25,29 @@ public final class R {
         public static final int sync=0x7f020007;
     }
     public static final class id {
-        public static final int account=0x7f070017;
-        public static final int buttonDeleteNote=0x7f070014;
+        public static final int ID_Policies_Scroll=0x7f07000f;
+        public static final int account=0x7f070018;
+        public static final int buttonDeleteNote=0x7f070015;
         public static final int buttonPixNote=0x7f070008;
         public static final int buttonSubmitLogin=0x7f07000c;
-        public static final int buttonSubmitNote=0x7f070013;
+        public static final int buttonSubmitNote=0x7f070014;
         public static final int buttonTextNote=0x7f070007;
-        public static final int buttonback=0x7f070010;
+        public static final int buttonback=0x7f070011;
         public static final int course=0x7f070002;
-        public static final int editSubmitNote=0x7f070012;
+        public static final int editSubmitNote=0x7f070013;
         public static final int editTextPassword=0x7f07000a;
         public static final int editTextUsername=0x7f070009;
         public static final int lectureDate=0x7f070004;
         public static final int linearLayout1=0x7f070006;
-        public static final int longP=0x7f07000f;
-        public static final int new_note=0x7f070015;
+        public static final int longP=0x7f070010;
+        public static final int new_note=0x7f070016;
         public static final int policies=0x7f07000e;
-        public static final int sync=0x7f070016;
+        public static final int sync=0x7f070017;
         public static final int text1=0x7f07000d;
         public static final int textView1=0x7f070000;
         public static final int textView2=0x7f070005;
         public static final int textView3=0x7f07000b;
-        public static final int textViewFilename=0x7f070011;
+        public static final int textViewFilename=0x7f070012;
         public static final int type=0x7f070003;
         public static final int welcometext=0x7f070001;
     }
